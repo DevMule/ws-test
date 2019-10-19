09735233d66f1821826625692bdbe774e7d04fce
DevMule <35449804+DevMule@users.noreply.github.com>
2019-10-20 00:43:19 +0500

    приложение морской бой: вёрстка и передача данных

--- app/app.js
+++ app/app.js
@@ -3,12 +3,14 @@ class App {
 		this.screen = document.getElementById('screen');
 
 		this.connection = new WebSocket('ws://localhost:8000');
-		this.connection.addEventListener('open', ()=>{});
+		this.connection.addEventListener('open', () => {
+		});
 		this.connection.addEventListener('message', this.onMessage.bind(this));
 
 		this.auth = new Authentication(this);
 		this.chat = new Chat(this);
 		this.folderSys = new FolderSystem(this);
+		this.battleshipGame = new Battleship(this);
 
 		this.openScreen(this.auth);
 	}
@@ -31,7 +33,11 @@ class App {
 
 			case 'auth':
 				if (event.value === 'success')
-					this.openScreen(this.chat);
+					this.openScreen(this.battleshipGame);
+				break;
+
+			case 'Battleship':
+				this.battleshipGame.onEvent(event);
 				break;
 
 			default:
@@ -51,6 +57,7 @@ class Service {
 	constructor(app) {
 		this.app = app;
 		this.elem = document.createElement('div');
+		this.elem.style.textAlign = 'center';
 	}
 }
 
@@ -75,7 +82,7 @@ class Chat extends Service {
 
 	wrireline(message) {
 		this.textarea.value += '\n' +
-			'['+ message.from +'] '+
+			'[' + message.from + '] ' +
 			message.data;
 	}
 
@@ -134,6 +141,245 @@ class Authentication extends Service {
 				pass: 'nopass',
 			});
 		}
+	}
+}
+
+class Battleship extends Service {
+	constructor(app) {
+		super(app);
+		// service ui
+		this.newGameButton = document.createElement('button');
+		this.newGameButton.innerHTML = 'new game';
+		this.newGameButton.addEventListener('click', this.onStartGameClick.bind(this));
+		this.resetBoardButton = document.createElement('button');
+		this.resetBoardButton.innerHTML = 'reset board';
+		this.resetBoardButton.addEventListener('click', this.resetBoard.bind(this));
+		this.gameStatusText = document.createElement('p');
+		this.gameStatusText.innerHTML = 'offline';
+		this.elem.appendChild(this.newGameButton);
+		this.elem.appendChild(this.resetBoardButton);
+		this.elem.appendChild(this.gameStatusText);
+
+		this.centerDiv = document.createElement('div');
+		this.centerDiv.style.margin = '0 auto';
+		this.elem.appendChild(this.centerDiv);
+
+		this.myBoard = this.getNewBoard();
+		this.centerDiv.appendChild(this.myBoard.board);
+		this.enemyBoard = this.getNewBoard();
+		this.centerDiv.appendChild(this.enemyBoard.board);
+
+		this.battleLog = document.createElement('textarea');
+		this.battleLog.disabled = true;
+		this.battleLog.style.width = '70%';
+		this.battleLog.style.height = '200px';
+		this.battleLog.style.resize = 'none';
+		this.battleLog.style.margin = '0 auto';
+		this.centerDiv.appendChild(this.battleLog);
+
+		// service values
+		this.myTurn = true;
+		this.gameStatus = "offline";
+	}
+
+	onEvent(event) {
+		switch (event.value) {
+			case 'new':
+				this.wrireline('new game, status: ' + event.gameStatus);
+				this.gameStatus =
+					this.gameStatusText.innerHTML = event.gameStatus;
+				if (event.gameStatus === 'online') {
+					this.wrireline(event.isYourTurn ? 'MY TURN' : 'ENEMY\'s TURN');
+					this.myTurn = event.isYourTurn;
+				}
+				this.resetBoardButton.disabled = true;
+				break;
+
+			case 'stop':
+				this.wrireline('game stopped!');
+				this.gameStatus =
+					this.gameStatusText.innerHTML = event.gameStatus;
+				this.resetBoardButton.disabled = false;
+				break;
+
+			case 'turn':
+				this.myTurn = true;
+				this.wrireline(`enemy\'s turn: ${event.col}, ${event.row}, now my turn`);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	onStartGameClick() {
+		switch (this.gameStatus) {
+			case "offline":
+				this.app.send({
+					type: "Battleship",
+					value: "new"
+				});
+				break;
+
+			case "online":
+				this.app.send({
+					type: "Battleship",
+					value: "stop"
+				});
+				break;
+
+			case "waiting":
+				this.app.send({
+					type: "Battleship",
+					value: "stop"
+				});
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	wrireline(message) {
+		this.battleLog.value += '\n' + message;
+	}
+
+	sendClick(col, row) {
+		this.app.send({
+			type: 'Battleship',
+			value: 'turn',
+			col: col,
+			row: row,
+		});
+	}
+
+	// board interface
+	onBoardClick(board, col, row) {
+		if (this.myTurn && board === this.enemyBoard) {
+			this.myTurn = false;
+			this.wrireline(`my turn: ${col}, ${row}, now enemy\'s turn`);
+			this.sendClick(col, row);
+		}
+	}
+
+	getNewBoard() {
+		let board = {};
+		board.chunks = [];
+		board.data = [];
+		board.board = document.createElement('TABLE');
+		board.board.style.margin = '40px';
+		board.board.style.display = 'inline-block';
+		for (let row = 0; row < 10; row++) {
+
+			board.chunks.push([]);
+			board.data.push([]);
+
+			let rowElem = document.createElement("TR");
+			board.board.appendChild(rowElem);
+
+			for (let col = 0; col < 10; col++) {
+
+				let colElem = document.createElement("TD");
+				colElem.style.margin =
+					colElem.style.padding = '0px';
+				rowElem.appendChild(colElem);
+
+				let btn = document.createElement("div");
+				btn.style.width =
+					btn.style.height = '25px';
+				btn.style.color =
+					btn.style.backgroundColor = '#999999';
+				colElem.appendChild(btn);
+
+				board.chunks[row].push(btn);
+				board.data[row].push(null);
+
+				btn.addEventListener('click', () => {
+					this.onBoardClick(board, col, row)
+				});
+			}
+		}
+		return board;
+	}
+
+	clearBoard(board) {
+		this.myships = [];
+		for (let row = 0; row < board.chunks.length; row++)
+			for (let col = 0; col < board.chunks[row].length; col++) {
+				let btn = board.chunks[row][col];
+				btn.style.color =
+					btn.style.backgroundColor = '#999999';
+				board.data[row][col] = null;
+			}
+	}
+
+	randomShip(size) {
+		let shipStart = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+		let shipShift = Math.random() > .5 ? [Math.random() > .5 ? 1 : -1, 0] : [0, Math.random() > .5 ? 1 : -1];
+		let ship = [];
+		for (let i = 0; i < size; i++) {
+			ship.push([shipStart[0], shipStart[1]]);
+			shipStart[0] += shipShift[0];
+			shipStart[1] += shipShift[1];
+		}
+		return ship;
+	}
+
+	isCollision(board, ship) {
+		for (let i = 0; i < ship.length; i++) {
+			let shipSegment = ship[i];
+
+			if (shipSegment[0] > 9 || shipSegment[0] < 0 || shipSegment[1] > 9 || shipSegment[1] < 0)
+				return true;
+
+			for (let row = -1; row < 2; row++) {
+				let checkRow = row + shipSegment[0];
+				if (checkRow >= 0 && checkRow <= 9)
+
+					for (let col = -1; col < 2; col++) {
+						let checkCol = col + shipSegment[1];
+						if (checkCol >= 0 && checkCol <= 9)
+							if (board.data[checkRow][checkCol])
+								return true;
+					}
+
+			}
+		}
+		return false;
+	}
+
+	placeShip(board, ship) {
+		for (let i = 0; i < ship.length; i++) {
+			let segment = ship[i];
+			board.data[segment[0]][segment[1]] = ship;
+			let btn = board.chunks[segment[0]][segment[1]];
+			btn.style.color =
+				btn.style.backgroundColor = '#ff9999';
+		}
+	}
+
+	randomizeShips(board) {
+		let shipsizes = [
+			4,
+			3, 3,
+			2, 2, 2,
+			1, 1, 1, 1,
+		];
+
+		for (let i = 0; i < shipsizes.length; i++) {
+			let collisionPass = false;
+			let ship = [];
+			while (!collisionPass) {
+				ship = this.randomShip(shipsizes[i]);
+				collisionPass = !this.isCollision(board, ship);
+			}
+			this.placeShip(board, ship);
+		}
+	}
+
+	resetBoard() {
+		this.clearBoard(this.myBoard);
+		this.randomizeShips(this.myBoard);
 	}
 }
 
--- index.js
+++ index.js
@@ -5,6 +5,36 @@ const WebSocket = require('ws');
 const StaticServer = require('static-server');
 StaticServer.createServer(__dirname + '\\app');
 
+let BattleshipMess = {
+	waits: [],
+	gameByPlayer: new Map(),
+	setGame: function (p1) {
+		if (this.waits[0] && this.waits[0] !== p1) {
+			let p2 = this.waits.pop();
+			let pair = [p1, p2, true];
+			this.gameByPlayer[p1] = pair;
+			this.gameByPlayer[p2] = pair;
+			return true;
+		} else {
+			this.waits.push(p1);
+			return false;
+		}
+	},
+	removeGame: function (p1) {
+		let game = this.gameByPlayer[p1];
+		this.gameByPlayer.delete(game[0]);
+		this.gameByPlayer.delete(game[1]);
+		return game;
+	},
+	isWait: function (p1) {
+		return this.waits.includes(p1)
+	},
+	stopWait: function (p1) {
+		for (let i = 0; i < this.waits.length; i++)
+			if (this.waits[i] === p1) this.waits.splice(i, 1);
+	}
+};
+
 // ws
 const wss = new WebSocket.Server({port: 8000});
 wss.on('connection', (ws) => {
@@ -26,11 +56,22 @@ wss.on('connection', (ws) => {
 			cli.send(JSON.stringify(res));
 		});
 		log(`${ws.name} disconnected!`);
+
+		// battleships
+		if (BattleshipMess.gameByPlayer[ws]) {
+			let game = BattleshipMess.removeGame(ws);
+			let otherPlayer = game[0] !== ws ? game[0] : game[1];
+			otherPlayer.send(JSON.stringify({
+				type: 'Battleship',
+				value: 'stop',
+				gameStatus: 'offline',
+			}));
+		}
+		if (BattleshipMess.isWait(ws)) BattleshipMess.stopWait(ws);
 	};
 
 	ws.onmessage = function (msg) {
 		msg = JSON.parse(msg.data);
-		log(msg.type);
 		switch (msg.type) {
 			case 'message':
 				var res = {
@@ -58,6 +99,57 @@ wss.on('connection', (ws) => {
 				};
 				ws.name = msg.name;
 				ws.send(JSON.stringify(res));
+				break;
+
+			case 'Battleship':
+				log(msg);
+				if (msg.value === 'new') {
+					if (BattleshipMess.gameByPlayer[ws])
+						BattleshipMess.removeGame(ws);
+					let isGame = BattleshipMess.setGame(ws);
+					let res = {
+						type: 'Battleship',
+						value: 'new',
+						gameStatus: isGame ? 'online' : 'waiting',
+						isYourTurn: false,
+					};
+
+					if (isGame) {
+						let game = BattleshipMess.gameByPlayer[ws];
+						game[0].send(JSON.stringify(res));
+						res.isYourTurn = true;
+						game[1].send(JSON.stringify(res));
+					} else {
+						ws.send(JSON.stringify(res));
+					}
+
+				} else if (msg.value === 'stop') {
+					let res = {
+						type: 'Battleship',
+						value: 'stop',
+						gameStatus: 'offline',
+					};
+
+					let iswait = BattleshipMess.isWait(ws);
+					if (iswait) {
+						BattleshipMess.stopWait(ws);
+						ws.send(JSON.stringify(res));
+					} else {
+						let game = BattleshipMess.removeGame(ws);
+						game[0].send(JSON.stringify(res));
+						game[1].send(JSON.stringify(res));
+					}
+				} else if (msg.value === 'turn') {
+					let game = BattleshipMess.gameByPlayer[ws];
+
+					// player can move - false - p1's move, else - p2's move
+					let [otherPlayer, pCanTurn] = game[0] !== ws ? [game[0], game[2]] : [game[1], !game[2]];
+
+					if (pCanTurn) {
+						otherPlayer.send(JSON.stringify(msg));
+						game[2] = !game[2];
+					}
+				}
 				break;
 
 			default:
